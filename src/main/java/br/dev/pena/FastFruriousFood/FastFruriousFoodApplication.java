3b38889db93186199ba1be920a7ec89bd623da5d
otavio <penasenai09@gmail.com>
2026-04-10 10:10:10 -0300

    incio da criação das classes Pedido e Produto

---
+++ src/main/java/br/dev/pena/FastFruriousFood/FastFruriousFoodApplication.java
@@ -0,0 +1,13 @@
+package br.dev.pena.FastFruriousFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFruriousFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFruriousFoodApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/dev/pena/FastFruriousFood/domain/model/Produto.java
@@ -0,0 +1,10 @@
+package br.dev.pena.FastFruriousFood.domain.model;
+
+public class Produto {
+    private long id;
+    private String nome;
+    private String CPF;
+    
+    
+    
+}
